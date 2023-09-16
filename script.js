7b5aa3a4b35f4aadca5ea688217a335c76b6f000
aprune <prunealin@gmail.com>
2023-09-16 23:01:38 +0200

    data base with phrases...

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-const textElement = document.getElementById('movingText');
+// Our "database" of phrases
+const phrases = [
+    "Sa ma sugi Cosmine.",
+    "Cel putin merge pagina.",
+    "Work in progress!",
+	"Get the fuck out already"
+	"Horse..."
+    // ... add more phrases as needed
+];
 
-function moveTextRandomly() {
-    // Fade out the text
-    textElement.style.opacity = 0;
-
-    // After the fade out duration (0.5s), move the text and fade it in
-    setTimeout(() => {
-        const x = Math.random() * (window.innerWidth - textElement.clientWidth);
-        const y = Math.random() * (window.innerHeight - textElement.clientHeight);
-
-        textElement.style.left = x + 'px';
-        textElement.style.top = y + 'px';
-
-        // Fade in the text
-        textElement.style.opacity = 1;
-    }, 500); // 500ms matches the CSS transition duration
+// Function to generate a random time between 1 to 2 seconds
+function getRandomTime() {
+    return Math.random() * 1000 + 1000; // 1000 to 2000 milliseconds
 }
 
-// Move and fade the text every 2 seconds
-setInterval(moveTextRandomly, 2000);
+// Function to move and fade a phrase element
+function moveAndFadePhrase(phraseElement) {
+    // Fade out the phrase
+    phraseElement.style.opacity = 0;
+
+    // After a random fade out duration, move the phrase and fade it in
+    setTimeout(() => {
+        const x = Math.random() * (window.innerWidth - phraseElement.clientWidth);
+        const y = Math.random() * (window.innerHeight - phraseElement.clientHeight);
+
+        phraseElement.style.left = x + 'px';
+        phraseElement.style.top = y + 'px';
+
+        // Fade in the phrase
+        phraseElement.style.opacity = 1;
+    }, getRandomTime());
+}
+
+// Create, style, and animate each phrase from the database
+phrases.forEach(phraseText => {
+    const phraseElement = document.createElement('div');
+    phraseElement.innerText = phraseText;
+    phraseElement.style.position = 'absolute';
+    phraseElement.style.fontWeight = 'bold';
+    phraseElement.style.fontSize = '2em';
+    phraseElement.style.transition = 'opacity 0.5s ease-out';
+
+    document.getElementById('phrasesContainer').appendChild(phraseElement);
+
+    // Move and fade the phrase at random intervals
+    setInterval(() => moveAndFadePhrase(phraseElement), getRandomTime());
+});
